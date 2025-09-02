2b4d87f32cc8f063a735df46d56ec745260b4211
bogdy <fratebogdy@gmail.com>
2025-09-02 10:35:45 +0300

    add variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
+    static class CubeRenderer {
+        float A = 0, B = 0, C = 0;
+
+        int width = 160, height = 44;
+        float[] zBuffer = new float[160 * 44];
+        char[] buffer = new char[160 * 44];
+
+        int backgroundASCIICode = ' ';
+        int distanceFromCam = 100;
+        float horizontalOffset;
+        float K1 = 40;
+        float incrementSpeed = 0.6f;
+    
+        
+    
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
