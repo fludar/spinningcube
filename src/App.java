7fb353159b5eb1af35fea93f8d24217849d3d0ab
bogdy <fratebogdy@gmail.com>
2025-09-02 10:40:53 +0300

    add render cube

--- src/App.java
+++ src/App.java
@@ -53,6 +53,21 @@ public class App {
             }
         }
         
+        void renderCube(float cubeWidth, float offset) {
+            horizontalOffset = offset;
+
+            for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+                for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+                    calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+                    calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+                    calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+                    calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+                    calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+                    calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+                }
+            }
+        }
+        
     }
     public static void main(String[] args) throws Exception {
         
