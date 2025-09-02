3ed7cc71db01197f96cba5924d80cbac3ea5074c
bogdy <fratebogdy@gmail.com>
2025-09-02 10:37:34 +0300

    calculate angles

--- src/App.java
+++ src/App.java
@@ -12,7 +12,27 @@ public class App {
         float K1 = 40;
         float incrementSpeed = 0.6f;
     
-        
+        float calculateX(float i, float j, float k) {
+            return (j * (float)Math.sin(A) * (float)Math.sin(B) * (float)Math.cos(C)
+                  - k * (float)Math.cos(A) * (float)Math.sin(B) * (float)Math.cos(C)
+                  + j * (float)Math.cos(A) * (float)Math.sin(C)
+                  + k * (float)Math.sin(A) * (float)Math.sin(C)
+                  + i * (float)Math.cos(B) * (float)Math.cos(C));
+        }
+
+        float calculateY(float i, float j, float k) {
+            return (j * (float)Math.cos(A) * (float)Math.cos(C)
+                  + k * (float)Math.sin(A) * (float)Math.cos(C)
+                  - j * (float)Math.sin(A) * (float)Math.sin(B) * (float)Math.sin(C)
+                  + k * (float)Math.cos(A) * (float)Math.sin(B) * (float)Math.sin(C)
+                  - i * (float)Math.cos(B) * (float)Math.sin(C));
+        }
+
+        float calculateZ(float i, float j, float k) {
+            return (k * (float)Math.cos(A) * (float)Math.cos(B)
+                  - j * (float)Math.sin(A) * (float)Math.cos(B)
+                  + i * (float)Math.sin(B));
+        }
     
     }
     public static void main(String[] args) throws Exception {
